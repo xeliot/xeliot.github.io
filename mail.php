39545fd921d869ab4e04f8ac0a495842d8e1fb2f
Dave Ho <daveho8888@gmail.com>
2019-09-10 21:37:31 -0700

    update me and skills

--- mail.php
+++ mail.php
@@ -1,6 +1,6 @@
 <?php
 
-	$emailTo = "product@aspirity.com"; // Enter your email for feedbacks here
+	$emailTo = "daveho8888@g.ucla.edu"; // Enter your email for feedbacks here
 
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=utf-8\r\n";
